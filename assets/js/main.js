0e7ee56313ace19064ed93843eb13577579d1c67
Farhan Tanvir Hridoy <mdfarhantanvir.buet17@gmail.com>
2025-07-25 11:39:44 +0600

    education section added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,119 +1,125 @@
 /* ----- NAVIGATION BAR FUNCTION ----- */
-    function myMenuFunction(){
-      var menuBtn = document.getElementById("myNavMenu");
+function myMenuFunction() {
+  var menuBtn = document.getElementById("myNavMenu");
 
-      if(menuBtn.className === "nav-menu"){
-        menuBtn.className += " responsive";
-      } else {
-        menuBtn.className = "nav-menu";
-      }
-    }
+  if (menuBtn.className === "nav-menu") {
+    menuBtn.className += " responsive";
+  } else {
+    menuBtn.className = "nav-menu";
+  }
+}
 
 /* ----- ADD SHADOW ON NAVIGATION BAR WHILE SCROLLING ----- */
-    window.onscroll = function() {headerShadow()};
+window.onscroll = function () { headerShadow() };
 
-    function headerShadow() {
-      const navHeader =document.getElementById("header");
+function headerShadow() {
+  const navHeader = document.getElementById("header");
 
-      if (document.body.scrollTop > 50 || document.documentElement.scrollTop >  50) {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
 
-        navHeader.style.boxShadow = "0 1px 6px rgba(0, 0, 0, 0.1)";
-        navHeader.style.height = "70px";
-        navHeader.style.lineHeight = "70px";
+    navHeader.style.boxShadow = "0 1px 6px rgba(0, 0, 0, 0.1)";
+    navHeader.style.height = "70px";
+    navHeader.style.lineHeight = "70px";
 
-      } else {
+  } else {
 
-        navHeader.style.boxShadow = "none";
-        navHeader.style.height = "90px";
-        navHeader.style.lineHeight = "90px";
+    navHeader.style.boxShadow = "none";
+    navHeader.style.height = "90px";
+    navHeader.style.lineHeight = "90px";
 
-      }
-    }
+  }
+}
 
 
 /* ----- TYPING EFFECT ----- */
-   var typingEffect = new Typed(".typedText",{
-      strings : ["Engineer","Teacher","Developer","Designer"],
-      loop : true,
-      typeSpeed : 100, 
-      backSpeed : 80,
-      backDelay : 2000
-   })
+var typingEffect = new Typed(".typedText", {
+  strings: ["Engineer", "Teacher", "Developer", "Designer"],
+  loop: true,
+  typeSpeed: 100,
+  backSpeed: 80,
+  backDelay: 2000
+})
 
 
 /* ----- ## -- SCROLL REVEAL ANIMATION -- ## ----- */
-   const sr = ScrollReveal({
-          origin: 'top',
-          distance: '80px',
-          duration: 2000,
-          reset: true     
-   })
+const sr = ScrollReveal({
+  origin: 'top',
+  distance: '80px',
+  duration: 2000,
+  reset: true
+})
 
-  /* -- HOME -- */
-  sr.reveal('.featured-text-card',{})
-  sr.reveal('.featured-name',{delay: 100})
-  sr.reveal('.featured-text-info',{delay: 200})
-  sr.reveal('.featured-text-btn',{delay: 200})
-  sr.reveal('.social_icons',{delay: 200})
-  sr.reveal('.featured-image',{delay: 300})
-  
+/* -- HOME -- */
+sr.reveal('.featured-text-card', {})
+sr.reveal('.featured-name', { delay: 100 })
+sr.reveal('.featured-text-info', { delay: 200 })
+sr.reveal('.featured-text-btn', { delay: 200 })
+sr.reveal('.social_icons', { delay: 200 })
+sr.reveal('.featured-image', { delay: 300 })
 
-  /* -- PROJECT BOX -- */
-  sr.reveal('.project-box',{interval: 200})
 
-  /* -- HEADINGS -- */
-  sr.reveal('.top-header',{})
+/* -- PROJECT BOX -- */
+sr.reveal('.project-box', { interval: 200 })
+
+/* -- HEADINGS -- */
+sr.reveal('.top-header', {})
 
 /* ----- ## -- SCROLL REVEAL LEFT_RIGHT ANIMATION -- ## ----- */
 
-  /* -- ABOUT INFO & CONTACT INFO -- */
-  const srLeft = ScrollReveal({
-    origin: 'left',
-    distance: '80px',
-    duration: 2000,
-    reset: true
-  })
-  
-  srLeft.reveal('.about-info',{delay: 100})
-  srLeft.reveal('.contact-info',{delay: 100})
+/* -- ABOUT INFO & CONTACT INFO -- */
+const srLeft = ScrollReveal({
+  origin: 'left',
+  distance: '80px',
+  duration: 2000,
+  reset: true
+})
 
-  /* -- ABOUT SKILLS & FORM BOX -- */
-  const srRight = ScrollReveal({
-    origin: 'right',
-    distance: '80px',
-    duration: 2000,
-    reset: true
-  })
-  
-  srRight.reveal('.skills-box',{delay: 100})
-  srRight.reveal('.form-control',{delay: 100})
-  
+srLeft.reveal('.about-info', { delay: 100 })
+srLeft.reveal('.contact-info', { delay: 100 })
+
+/* -- ABOUT SKILLS & FORM BOX -- */
+const srRight = ScrollReveal({
+  origin: 'right',
+  distance: '80px',
+  duration: 2000,
+  reset: true
+})
+
+srRight.reveal('.skills-box', { delay: 100 })
+srRight.reveal('.form-control', { delay: 100 })
+
 
 
 /* ----- CHANGE ACTIVE LINK ----- */
-  
-  const sections = document.querySelectorAll('section[id]')
 
-  function scrollActive() {
-    const scrollY = window.scrollY;
+const sections = document.querySelectorAll('section[id]')
 
-    sections.forEach(current =>{
-      const sectionHeight = current.offsetHeight,
-          sectionTop = current.offsetTop - 50,
-        sectionId = current.getAttribute('id')
+function scrollActive() {
+  const scrollY = window.scrollY;
 
-      if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) { 
+  sections.forEach(current => {
+    const sectionHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 50,
+      sectionId = current.getAttribute('id')
 
-          document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.add('active-link')
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
 
-      }  else {
+      document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.add('active-link')
 
-        document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.remove('active-link')
+    } else {
 
-      }
-    })
-  }
+      document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.remove('active-link')
 
-  window.addEventListener('scroll', scrollActive)
+    }
+  })
+}
 
+window.addEventListener('scroll', scrollActive)
+
+
+
+// Dark mode toggle
+document.getElementById('darkModeToggle').onclick = function () {
+  document.body.classList.toggle('dark-mode');
+};
 
